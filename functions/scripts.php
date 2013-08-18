61f29885a983c7035a0586704d0db4c73bdcf48c
Scott Lyttle <scott.lyttle@gmail.com>
2013-08-18 21:27:41 +1000

    Enqueued WP's bundled version of jQuery

--- functions/scripts.php
+++ functions/scripts.php
@@ -1,6 +1,7 @@
 <?php
 // Load other custom scripts
 function blankwp_scripts() {
+	wp_enqueue_script('jquery');
 	wp_enqueue_script('functions', get_template_directory_uri() . '/js/functions.js', array('jquery'));
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
